77347a6219f435b0e45b27f76d6e3dc5982d6045
Bartek Karelus <bartek.karelus@gmail.com>
2020-08-21 19:55:03 +0200

    install propTypes / change function component for class component / use PropTypes

--- src/container/burgerBuilder/BurgerElements/BurgerElements.js
+++ src/container/burgerBuilder/BurgerElements/BurgerElements.js
@@ -1,31 +1,41 @@
-import React from "react";
-import burgerElements from "./burgerElements.css";
-const burgerElements = (props) => {
-  let elementsOfBurger = null;
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import BurgerElements from "./burgerElements.css";
 
-  switch (props.type) {
-    case "burger-bottom":
-      elementsOfBurger = <div className={"BreadBottom"}></div>;
-      break;
-    case "burger-top":
-      elementsOfBurger = (
-        <div className={"BreadTop"}>
-          <div className={"Seeds1"}></div>
-          <div className={"Seeds2"}></div>
-          <div className={"Salad"}></div>
-        </div>
-      );
-      break;
-    case "meat":
-      elementsOfBurger = <div className={"Meat"}></div>;
-      break;
-    case "Bacon":
-      elementsOfBurger = <div className={"Bacon"}></div>;
-      break;
-    case "Cheese":
-      elementsOfBurger = <div className={"Cheese"}></div>;
-      break;
+class BurgerElements extends Component {
+  render() {
+    let elementsOfBurger = null;
+
+    switch (this.props.type) {
+      case "burger-bottom":
+        elementsOfBurger = <div className={"BreadBottom"}></div>;
+        break;
+      case "burger-top":
+        elementsOfBurger = (
+          <div className={"BreadTop"}>
+            <div className={"Seeds1"}></div>
+            <div className={"Seeds2"}></div>
+            <div className={"Salad"}></div>
+          </div>
+        );
+        break;
+      case "meat":
+        elementsOfBurger = <div className={"Meat"}></div>;
+        break;
+      case "Bacon":
+        elementsOfBurger = <div className={"Bacon"}></div>;
+        break;
+      case "Cheese":
+        elementsOfBurger = <div className={"Cheese"}></div>;
+        break;
+    }
+    return elementsOfBurger;
   }
-};
+}
 
-export default burgerElements;
+BurgerElements.PropTypes = {
+  type = PropTypes.string.isRequired
+}
+
+
+export default BurgerElements;
