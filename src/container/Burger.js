7fb2dc4271c6631f2cdcef0a52ae2fcfa256c105
Bartek Karelus <bartek.karelus@gmail.com>
2020-08-24 21:03:01 +0200

    added  state, added const which return burgerElement with keys and type

--- src/container/Burger.js
+++ src/container/Burger.js
@@ -2,6 +2,14 @@ import React from "react";
 import "./Burger.css";
 import BurgerElements from "../container/burgerBuilder/BurgerElements/BurgerElements";
 const burger = (props) => {
+  const transformIngriedents = Object.keys(props.ingriedents).map((el) => {
+    return [...Array(props.ingriedents[el])].map((_, i) => {
+      return <BurgerElements key={el + i} type={el} />;
+    });
+  });
+
+  console.log(transformIngriedents);
+
   return (
     <div className="Burger">
       <BurgerElements type="burger-top" />
--- src/container/burgerBuilder/burgerBuilder.js
+++ src/container/burgerBuilder/burgerBuilder.js
@@ -2,10 +2,18 @@ import React, { Component } from "react";
 import Burger from "../Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingriedents: {
+      salad: 1,
+      meat: 1,
+      bacon: 2,
+      cheese: 2,
+    },
+  };
   render() {
     return (
       <div>
-        <Burger />
+        <Burger ingriedents={this.state.ingriedents} />
         <div>Burger buttons</div>
       </div>
     );
